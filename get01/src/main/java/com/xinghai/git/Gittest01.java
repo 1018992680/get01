b2696bae5557d66f0f98a6e90afe873fe295b1a3
zcy <1018992680@qq.com>
2022-07-10 20:04:26 +0800

    test03

--- get01/src/main/java/com/xinghai/git/Gittest01.java
+++ get01/src/main/java/com/xinghai/git/Gittest01.java
@@ -5,5 +5,6 @@ public class Gittest01 {
     public static void main(String[] args){
         System.out.println("哈喽...");
         System.out.println("在吗？");
+        System.out.println("在的~");
     }
 }
