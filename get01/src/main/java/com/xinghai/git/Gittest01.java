9c57bd1727594b60218f04ce687ced314d0519d4
zcy <1018992680@qq.com>
2022-07-14 10:22:03 +0800

    gitee-01

--- get01/src/main/java/com/xinghai/git/Gittest01.java
+++ get01/src/main/java/com/xinghai/git/Gittest01.java
@@ -11,5 +11,6 @@ public class Gittest01 {
         System.out.println("我也是..");
         System.out.println("嗯嗯~");
         System.out.println("嘿嘿...嘿嘿!");
+        System.out.println("亲爱的，妳跟我飞，小心前面带刺的玫瑰。亲爱的，妳慢慢飞，飞过花香去看小溪水");
     }
 }
